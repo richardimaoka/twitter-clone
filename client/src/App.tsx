244f4612143faf4bab2307089366e1ddb62f2309
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-05-31 23:30:49 +0900

    three columns

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,22 @@ import React from "react";
 
 function App() {
   return (
-    <>
-      <section>left pane</section>
+    <div style={{ display: "flex" }}>
+      <section>
+        <div>logo</div>
+        <div>home</div>
+        <div>話題を検索</div>
+        <div>通知</div>
+        <div>メッセージ</div>
+        <div>ブックマーク</div>
+        <div>リスト</div>
+        <div>プロフィール</div>
+        <div>もっと見る</div>
+        <div>ツイートする</div>
+      </section>
       <main>main</main>
       <section>right pane</section>
-    </>
+    </div>
   );
 }
 
