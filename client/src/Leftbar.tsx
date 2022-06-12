a0132e5234f78a88f09070b6fbc126aa5e9fae9a
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-06-13 04:52:30 +0900

    home button refined

--- client/src/Leftbar.tsx
+++ client/src/Leftbar.tsx
@@ -17,7 +17,7 @@ export const Leftbar = (): JSX.Element => {
   return (
     <section
       css={css`
-        background-color: #c0c0c0;
+        background-color: #f3f3f3;
         margin-left: auto;
         @media (min-width: 1280px) {
           width: 270px;
@@ -27,31 +27,67 @@ export const Leftbar = (): JSX.Element => {
       <div>
         <FontAwesomeIcon icon={faDove} size="2x" style={{ color: "#6163d4" }} />
       </div>
-      <div>
-        <FontAwesomeIcon
-          icon={faHouse}
-          size="2x"
-          style={{ color: "#6163d4" }}
-        />
-        home
+
+      <div
+        css={css`
+          display: flex;
+          align-items: center;
+        `}
+      >
+        <div
+          css={css`
+            width: 24px;
+            height: 24px;
+            display: flex;
+            justify-items: center;
+            align-items: center;
+          `}
+        >
+          <FontAwesomeIcon
+            icon={faHouse}
+            size="lg"
+            style={{ color: "#252525" }}
+          />
+        </div>
+        <div>
+          <span
+            css={css`
+              font-size: 18px;
+            `}
+          >
+            home
+          </span>
+        </div>
+      </div>
+
+      <div
+        css={css`
+          display: flex;
+        `}
+      >
+        <div
+          css={css`
+            width: 50px;
+            height: 50px;
+          `}
+        >
+          <FontAwesomeIcon
+            icon={faHashtag}
+            size="2x"
+            style={{ color: "#252525" }}
+          />
+        </div>
+        <div>話題を検索</div>
       </div>
       <div>
-        <FontAwesomeIcon
-          icon={faHashtag}
-          size="2x"
-          style={{ color: "#6163d4" }}
-        />
-        話題を検索
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faBell} size="2x" style={{ color: "#6163d4" }} />
+        <FontAwesomeIcon icon={faBell} size="2x" style={{ color: "#252525" }} />
         通知
       </div>
       <div>
         <FontAwesomeIcon
           icon={faMessage}
           size="2x"
-          style={{ color: "#6163d4" }}
+          style={{ color: "#252525" }}
         />
         メッセージ
       </div>
@@ -59,7 +95,7 @@ export const Leftbar = (): JSX.Element => {
         <FontAwesomeIcon
           icon={faBookmark}
           size="2x"
-          style={{ color: "#6163d4" }}
+          style={{ color: "#252525" }}
         />
         ブックマーク
       </div>
@@ -67,19 +103,19 @@ export const Leftbar = (): JSX.Element => {
         <FontAwesomeIcon
           icon={faClipboardList}
           size="2x"
-          style={{ color: "#6163d4" }}
+          style={{ color: "#252525" }}
         />
         リスト
       </div>
       <div>
-        <FontAwesomeIcon icon={faUser} size="2x" style={{ color: "#6163d4" }} />
+        <FontAwesomeIcon icon={faUser} size="2x" style={{ color: "#252525" }} />
         プロフィール
       </div>
       <div>
         <FontAwesomeIcon
           icon={faEllipsis}
           size="2x"
-          style={{ color: "#6163d4" }}
+          style={{ color: "#252525" }}
         />
         もっと見る
       </div>
